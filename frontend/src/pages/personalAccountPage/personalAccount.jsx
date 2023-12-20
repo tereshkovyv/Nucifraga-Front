57f67d5c4bdbf1e0fbb34438883afd50f9d96501
MarkovGerman <lllkdodobdvakk@gmail.com>
2023-12-20 20:48:31 +0500

    Add header

--- frontend/src/pages/personalAccountPage/personalAccount.jsx
+++ frontend/src/pages/personalAccountPage/personalAccount.jsx
@@ -7,7 +7,7 @@ const pointerBlackImg = "img/images/imgPersonalAccount/pointerBlack.png";
 const iconPersonMiniImg = "img/images/imgPersonalAccount/icoUserMini.png";
 const timeIconImg = "img/images/imgPersonalAccount/timeIcon.png";
 
-const PersonalAccount = () => {
+const PersonalAccountNew = () => {
 
     const [boards, setBoards] = useState([
         { id: 1, title:"Идеи", items: [{id: 1, title: "Изучить технологию изготовления asdjhfaswojdfo " +
@@ -22,7 +22,10 @@ const PersonalAccount = () => {
                 {id: 8, title: "3", data: "27 jun" },
                 {id: 9, title: "4", data: "27 jun" },
                 {id: 10, title: "5", data: "27 jun" },
-                {id: 11, title: "6", data: "27 jun" }
+                {id: 11, title: "6", data: "27 jun" },
+                {id: 12, title: "7", data: "27 jun" },
+                {id: 13, title: "8", data: "27 jun" },
+                {id: 14, title: "9", data: "27 jun" }
             ]}
     ]);
 
@@ -65,21 +68,78 @@ const PersonalAccount = () => {
 
         tempCurrentPages.forEach((currentPage) => {
             if (currentPage.id === id) {
-                tempCurrentPages[currentPage.id - 1].currentBlock = pageNumber[0];
+                tempCurrentPages[currentPage.id - 1].currentBlock = pageNumber;
             }
         })
 
         return tempCurrentPages;
     }
 
-    const paginate = (pageNumber, id) => setCurrentPages(getChangeList(pageNumber, id));
+    function getNextChangeList(id, boardItems) {
+        let tempCurrentPages = currentPages.slice(0);
+
+        tempCurrentPages.forEach((currentPage) => {
+            if (currentPage.id === id) {
+                if (tempCurrentPages[currentPage.id - 1].currentBlock + 1 <= Math.ceil(boardItems.length / taskPerPages)) {
+                    tempCurrentPages[currentPage.id - 1].currentBlock += 1;
+                }
+            }
+        })
+
+        return tempCurrentPages;
+    }
+
+    function getPreviousChangeList(id) {
+        let tempCurrentPages = currentPages.slice(0);
+
+        tempCurrentPages.forEach((currentPage) => {
+            if (currentPage.id === id) {
+                if (tempCurrentPages[currentPage.id - 1].currentBlock - 1 > 0) {
+                    tempCurrentPages[currentPage.id - 1].currentBlock -= 1;
+                }
+            }
+        })
+
+        return tempCurrentPages;
+    }
+
+    function getStartChangeList(id) {
+        let tempCurrentPages = currentPages.slice(0);
+
+        tempCurrentPages.forEach((currentPage) => {
+            if (currentPage.id === id) {
+                tempCurrentPages[currentPage.id - 1].currentBlock = 1;
+            }
+        })
+
+        return tempCurrentPages;
+    }
+
+    function getEndChangeList(id, boardItems) {
+        let tempCurrentPages = currentPages.slice(0);
+
+        tempCurrentPages.forEach((currentPage) => {
+            if (currentPage.id === id) {
+                tempCurrentPages[currentPage.id - 1].currentBlock = Math.ceil(boardItems.length / taskPerPages);
+            }
+        })
+
+        return tempCurrentPages;
+    }
+
+    const paginate = (pageNumber, id) => setCurrentPages(getChangeList(pageNumber[0], id));
+    const nextList = (id, boardItems) => setCurrentPages(getNextChangeList(id, boardItems));
+    const previousList = id => setCurrentPages(getPreviousChangeList(id));
+    const startList = id => setCurrentPages(getStartChangeList(id));
+    const endList = (id, boardItems) => setCurrentPages(getEndChangeList(id, boardItems));
+
 
     function dragOverHandler(e) {
         e.preventDefault();
     }
 
     function dragLeaveHandler(e) {
-        
+
     }
 
     function dragStartHandler(e, board, item) {
@@ -87,11 +147,14 @@ const PersonalAccount = () => {
         setCurrentItem(item);
     }
 
-    function dragEndHandler(e) { }
+    function dragEndHandler(e) {
+
+    }
 
     function dragHandler(e, board, item) {
         e.preventDefault();
         e.stopPropagation();
+
         const currentIndex = currentBoard.items.indexOf(currentItem);
         currentBoard.items.splice(currentIndex, 1);
 
@@ -108,9 +171,9 @@ const PersonalAccount = () => {
             return b;
         }))
     }
+
     function dropCardHandler(e, board) {
         board.items.push(currentItem);
-        currentItem.status = board.id;
         const currentIndex = currentBoard.items.indexOf(currentItem);
         currentBoard.items.splice(currentIndex, 1);
 
@@ -151,8 +214,8 @@ const PersonalAccount = () => {
                 <div className={"personal__account__info__blocks"}>
                     {boards.map((board) => (
                         <div onDragOver={(e) => dragOverHandler(e)}
-                            onDrop={(e) => dropCardHandler(e, board)}
-                            className={"personal__account__info__blocks__text"}>
+                             onDrop={(e) => dropCardHandler(e, board)}
+                             className={"personal__account__info__blocks__text"}>
 
                             <text>
                                 {board.title}
@@ -160,15 +223,15 @@ const PersonalAccount = () => {
 
                             {getTaskPerBlock(board).map((item) => (
                                 <div onDragOver={(e) => dragOverHandler(e)}
-                                    onDragLeave={(e) => dragLeaveHandler(e)}
-                                    onDragStart={(e) => dragStartHandler(e, board, item)}
-                                    onDragEnd={(e) => dragEndHandler(e)}
-                                    onDrop={(e) => dragHandler(e, board, item)}
-                                    draggable={"true"}
-                                    className={"personal__account__info__blocks__task"}>
+                                     onDragLeave={(e) => dragLeaveHandler(e)}
+                                     onDragStart={(e) => dragStartHandler(e, board, item)}
+                                     onDragEnd={(e) => dragEndHandler(e)}
+                                     onDrop={(e) => dragHandler(e, board, item)}
+                                     draggable={"true"}
+                                     className={"personal__account__info__blocks__task"}>
 
                                     <text>
-                                        {item.name}
+                                        {item.title}
                                     </text>
 
                                     <div className={"personal__account__info__blocks__footer"}>
@@ -180,18 +243,24 @@ const PersonalAccount = () => {
                                         <div className={"personal__account__info__blocks__data"}>
                                             <img src={timeIconImg} alt={"timeIconImg"}/>
                                             <text>
-                                                {item.time != null ? item.time.split('T')[0]:""}
+                                                {item.data}
                                             </text>
                                         </div>
                                     </div>
                                 </div>
                             ))}
                             <div className={"pagination"}>
+                                <label  onClick={() => startList(board.id)}>В начало</label>
+
+                                <label onClick={() => previousList(board.id)} >&lt;</label>
                                 {getPagination(taskPerPages, board.items.length).map((number) =>
                                     <div>
-                                        <label onClick={() => paginate(number, board.id)} >{number}</label>
+                                        <label onClick={() => paginate(number, board.id)} > {number} </label>
                                     </div>
                                 )}
+                                <label onClick={() => nextList(board.id, board.items)}>&#707;</label>
+
+                                <label  onClick={() => endList(board.id, board.items)}>В конец</label>
                             </div>
                         </div>
                     ))}
@@ -200,7 +269,7 @@ const PersonalAccount = () => {
 
             <div className={"personal__account__info__create__block"}>
                 <div className={"personal__account__info__create__block-btn"}>
-                    <a href="#!">Создать</a>
+                    <a href="/createTask">Создать</a>
                 </div>
             </div>
 
@@ -241,4 +310,4 @@ const PersonalAccount = () => {
     );
 }
 
-export default PersonalAccount;
+export default PersonalAccountNew;
